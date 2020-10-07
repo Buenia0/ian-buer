2cc5686eaf91e55b22a727e1ad34bd0d5a32a492
Buenia0 <buenia.mbemu@gmail.com>
2020-10-06 19:06:16 -0500

    Completed Activity 4.

---
+++ activity-4/js/scripts.js
@@ -0,0 +1,15 @@
+var user = 'Ian';
+var salutation = 'Hello, ';
+var greeting = salutation + user + '! Here are the latest Coke reviews.'
+var greetingEl = document.getElementById('greeting');
+
+greetingEl.textContent = greeting;
+
+var price = 1.99,
+    studentDiscount = .10,
+    studentPrice = price - (price * studentDiscount),
+    priceEl = document.getElementById('price');
+    studentPriceEl = document.getElementById('student-price');
+
+priceEl.textContent = price.toFixed(2);
+studentPriceEl.textContent = studentPrice.toFixed(2);
